f42fa7c1f13040ac01c694cf52cac3f6d15c2a0c
Temoto-kun <allan.crisostomo@pup.edu.ph>
2013-11-16 18:34:51 +0800

    appcontroller revert

---
+++ app/Controller/AppController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Alvin
+ * Date: 11/16/13
+ * Time: 11:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AppController extends Controller {
+
+}
